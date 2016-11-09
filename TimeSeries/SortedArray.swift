cd288a6ccae7478be1e0a5c34cd280e4303a9ebf
Adam Nemecek <adamnemecek>
2016-11-08 17:02:15 -0800

    * initial commit

---
+++ TimeSeries/SortedArray.swift
@@ -0,0 +1,99 @@
+//
+//  SortedArray.swift
+//  ATWrappers
+//
+//  Created by Adam Nemecek on 10/1/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+import Foundation
+
+//protocol OrderedCollection: Collection {
+//  associatedtype _Element: Comparable = Iterator.Element
+//}
+
+struct SortedArray<Element: Comparable> : MutableCollection, RangeReplaceableCollection, /* RandomAccessCollection,OrderedCollection,*/ ExpressibleByArrayLiteral,  BidirectionalCollection {
+  private var content: [Element]
+
+  public init() {
+    content = []
+  }
+
+  init<S: Sequence>(_ seq: S) where S.Iterator.Element == Element {
+    content = seq.sorted()
+  }
+
+  public init(arrayLiteral elements: Element...) {
+    content = elements.sorted()
+  }
+
+  public var startIndex: Int {
+    return content.startIndex
+  }
+
+  public var endIndex: Int {
+    return content.endIndex
+  }
+
+  subscript (index: Int) -> Element {
+    get {
+      return content[index]
+    }
+    set {
+      content[index] = newValue
+    }
+  }
+
+  func index(after index: Int) -> Int {
+    return index + 1
+  }
+
+  func index(before index: Int) -> Int {
+    return index - 1
+  }
+
+  public mutating
+  func replaceSubrange<C: Collection>(_ subrange: Range<Int>, with newElements: C) where C.Iterator.Element == Element {
+    //
+    // todo: optimize
+    //
+    content.replaceSubrange(subrange, with: newElements)
+    content = content.sorted()
+  }
+
+  mutating
+  func append<S: Sequence>(contentsOf newElements: S) where S.Iterator.Element == Element {
+    content.append(contentsOf: newElements)
+    content = content.sorted()
+  }
+
+
+  func index(of element: Element) -> Int? {
+//    fatalError("implement")
+    return content.index(of: element)
+//    return nil
+  }
+
+  func sorted() -> [Element] {
+    return content
+  }
+
+  func max() -> Element? {
+    return content.last
+  }
+
+  func min() -> Element? {
+    return content.first
+  }
+}
+
+//extension SortedArray where Element: Hashable {
+//  mutating func elementsEqual<S: Sequence>(_ other: S, by areEquivalent: (Element, Element) throws -> Bool) rethrows -> Bool where S.Iterator.Element == Element {
+//    fatalError()
+//  }
+//}
+
+
+
+
+
---
+++ TimeSeries/TimeSeries.swift
@@ -0,0 +1,167 @@
+//
+//  TimeSeries.swift
+//  TimeSeries
+//
+//  Created by Adam Nemecek on 11/8/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+import Foundation
+/*
+protocol TemporalIntegral: Temporal, ExpressibleByIntegerLiteral { }
+
+struct TimeSeriesIndex<Element: TemporalIntegral>: Comparable, DefaultConstructible, Temporal, ExpressibleByIntegerLiteral {
+  let timestamp: Element.Time
+  //
+  // this is the offset from (timestamp, 0)
+  //
+  fileprivate let offset: Int
+
+  //
+  // this is the linear index into the sorted array
+  //
+  fileprivate let index: Int?
+
+  init(timestamp: Element.Time = Element.Time(), offset: Int = 0, index: Int? = nil) {
+    self.timestamp = timestamp
+    self.offset = offset
+    self.index = index
+  }
+
+  init(integerLiteral value: IntegerLiteralType) {
+    self.init(timestamp: Event.Time(integerLiteral: value))
+  }
+
+  static var min: TimeSeriesIndex {
+    return TimeSeries1Index(timestamp: Element.Time.min, offset: 0)
+  }
+
+  static var max: TimeSeriesIndex {
+    return TimeSeries1Index(timestamp: Element.Time.max, offset: 0)
+  }
+}
+
+
+func ==<T>(lhs: TimeSeries1Index<T>, rhs: TimeSeries1Index<T>) -> Bool {
+  return lhs.timestamp == rhs.timestamp && lhs.index == rhs.index
+}
+
+func <<T>(lhs: TimeSeries1Index<T>, rhs: TimeSeries1Index<T>) -> Bool {
+  return (lhs.timestamp < rhs.timestamp && lhs.index < rhs.index) ||
+         (lhs.timestamp != T.Time.max && rhs.timestamp == T.Time.max)
+}
+
+extension Sequence {
+  func count(where: (Iterator.Element) -> Bool) -> Int {
+    return reduce(0) { $0 + Int(predicate($1)) }
+  }
+}
+
+
+extension SortedArray where Element: Temporal  {
+  func indices(of domain: Range<Element.Time>) -> Range<Int> {
+
+  }
+
+  func count(at timestamp: Element.Time) -> Int {
+    fatalError()
+    //return reduce(0) { $0 + Int($1.timestamp == timestamp) }
+    return count { $0.timestamp == timestamp }
+  }
+
+  func count(until timestamp: Element.Time) -> Int {
+    fatalError()
+//    return reduce(0) { $0 + Int($1.timestamp < timestamp) }
+    return count { $0.timestamp < timestamp }
+  }
+
+  func count(from timestamp: Element.Time) -> Int {
+//    return reduce(0) { $0 + Int(timestamp < $1.timestamp) }
+    fatalError()
+  }
+
+}
+
+//extension BidirectionalCollection {
+//}
+
+extension Range {
+	init(bound: Bound) {
+		self.init(bound..<bound)
+	}
+}
+
+
+struct TimeSeries<Event: Temporal>: BidirectionalCollection, RangeReplaceableCollection, MutableCollection {
+
+  private var content: SortedArray<Event> = []
+  typealias Index = TimeSeriesIndex<Event>
+  typealias SubSequence = Slice<SortedArray<Event>>
+
+  var domain: Range<Event.Time> {
+    return first.map { $0.timestamp...Event.Time.max } ?? Event.Time()..<Event.Time()
+  }
+
+  //
+  // calculate linear index
+  //
+  private func lindex(timestamp: Event.Time, offset: Int) -> Int {
+    return content.count(until: index.timestamp) + offset
+  }
+
+  private func lindex(index: Index) -> Int {
+    return index.index ?? lindex(timestamp: index.timestamp, offset: index.offset)
+  }
+
+  private func linrange(bounds: Range<Index>) -> Range<Int> {
+    return lindex(index: bounds.lowerBound)..<lindex(index: bounds.upperBound)
+  }
+
+
+  private func index(for index: Int) -> Index {
+    let event = content[offset]
+    let until = content.count(until: event.timestamp)
+
+    return Index(timestamp: event.timestamp, offset: index - until, index: index)
+  }
+
+  var startIndex: Index {
+    return Index(timestamp: domain.lowerBound, index: 0, linearIndex: 0)
+  }
+
+  var endIndex: Index {
+    //
+    // note that upperBound might be Evnet.Time.max'
+    //
+    return Index(timestamp: domain.upperBound, index: 0, linearIndex: content.endIndex)
+  }
+
+  func index(after index: Index) -> Index {
+    let idx = lindex(index: index) + 1
+    return idx < content.endIndex ? index(for: idx) : endIndex
+  }
+
+  subscript (index: Index) -> Event {
+    get {
+      return content[lindex(index: index)]
+    }
+    set {
+        content[lindex(index: index)] = newValue
+    }
+  }
+
+//  subscript (bounds: Range<Index>) -> SubSequence {
+//    fatalError()
+//  }
+
+  func makeIterator() -> AnyIterator<Event> {
+    return content.makeIterator()
+  }
+
+  mutating
+  func replaceSubrange<C : Collection>(_ subrange: Range<Index>, with newElements: C) where C.Iterator.Element == Event {
+    let r = linrange(bounds: subrange)
+    content.replaceSubRange(r, newElements)
+  }
+
+}*/
