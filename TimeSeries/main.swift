e6a3875271bc721b46bc583801f2ed45178397ce
Adam Nemecek <adamnemecek>
2016-11-08 16:51:51 -0800

    Initial Commit

---
+++ TimeSeries/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TimeSeries
+//
+//  Created by Adam Nemecek on 11/8/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
